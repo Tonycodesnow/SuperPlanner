a983d059ee0c0bd8ffabc5905f48a7ea4ade9312
Ant <huertinez.family@outlook.com>
2021-09-01 07:15:59 -0700

    added html bootstrap features

---
+++ assets/js/script.js
@@ -0,0 +1,77 @@
+// var to store loop arrays
+var eventData = [
+    {
+        hour "9",
+        daytime: "am",
+        data: ""
+    },
+    {
+        hour "10",
+        daytime: "am",
+        data: ""
+        
+    },
+    {
+        hour "11",
+        daytime: "am",
+        data: ""
+    },
+    {
+        hour "12",
+        daytime: "pm",
+        data: ""
+    },
+    {
+        hour "1",
+        daytime: "pm",
+        data: ""
+    },
+    {
+        hour "2",
+        daytime: "pm",
+        data: ""
+    },
+    {
+        hour "3",
+        daytime: "pm",
+        data: ""
+    },
+    {
+        hour "4",
+        daytime: "pm",
+        data: ""
+    },
+    {
+        hour "5",
+        daytime: "pm",
+        data: ""
+    },
+    
+]
+// data for header date
+var present = moment().format('')
+
+
+// save data for local storage
+
+
+// set data from local storage to view
+
+
+
+// save button
+
+
+
+// load header date
+
+
+
+// scheduler data
+
+
+// create visuals for scheduler body
+
+
+
+// create save button
