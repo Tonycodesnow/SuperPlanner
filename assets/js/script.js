e419a2177bc5253a0ae3e26eced9168ab925c1b7
Ant <huertinez.family@outlook.com>
2021-09-01 20:37:02 -0700

    updated eventData, added timeBlock function, began local storage cleaned up HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,77 +1,106 @@
 // var to store loop arrays
 var eventData = [
     {
-        hour "9",
+        hour: "9",
         daytime: "am",
-        data: ""
+        altHour: 9
     },
     {
-        hour "10",
+        hour: "10",
         daytime: "am",
-        data: ""
-        
+        altHour: 10
+
     },
     {
-        hour "11",
+        hour: "11",
         daytime: "am",
-        data: ""
+        altHour: 11
     },
     {
-        hour "12",
+        hour: "12",
         daytime: "pm",
-        data: ""
+        altHour: 12
     },
     {
-        hour "1",
+        hour: "1",
         daytime: "pm",
-        data: ""
+        altHour: 13
     },
     {
-        hour "2",
+        hour: "2",
         daytime: "pm",
-        data: ""
+        altHour: 14
     },
     {
-        hour "3",
+        hour: "3",
         daytime: "pm",
-        data: ""
+        altHour: 15
     },
     {
-        hour "4",
+        hour: "4",
         daytime: "pm",
-        data: ""
+        altHour: 16
     },
     {
-        hour "5",
+        hour: "5",
         daytime: "pm",
-        data: ""
+        altHour: 17
     },
     
 ]
+
+function addTimeBlock() {
+    for (var i = 0; i < eventData.length; i++) {
+        var time = eventData[i].hour+eventData[i].daytime
+        var timeBlock = $("<div>").addClass("row");
+
+        var hour = $("<div>").addClass("col-md-2 hour").text(time);
+        var description = $("<textarea>").addClass("description col-md-9").attr("id", eventData[i].altHour);
+
+        var saveBtn = $("<button>").addClass("saveBtn col-md-1");
+        
+        var icon = $("<i>").addClass("fas fa-save");
+        saveBtn.append(icon)
+        timeBlock.append(hour)
+        timeBlock.append(description)
+        timeBlock.append(saveBtn)
+        // change color here
+
+        $(".container").append(timeBlock)
+    }
+}
 // data for header date
-var present = moment().format('')
-
-
-// save data for local storage
-
-
-// set data from local storage to view
-
-
-
-// save button
+var now = moment()
 
 
 
 // load header date
+
+var displayTime = function(Time) {
+    
+}
+// save data for local storage
+// need to know what i can save
+$(".saveBtn").on("click", function(){
+    var value = $(this).siblings(".description").val()
+    var time = $(this).siblings(".description").attr("id")
+    localStorage.setItem(time,value)
+})
+
+// set data from local storage to view
+
+$(".9").val(localStorage.getItem("9"))
+// make a loop or write out multiple times
+// save button
 
 
 
 // scheduler data
 
 
+
 // create visuals for scheduler body
 
 
-
+addTimeBlock()
 // create save button
